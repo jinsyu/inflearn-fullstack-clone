e176ca2a733b88b473ef91e82de4335dea175545
jinsyu <jinsyu.com@gmail.com>
2025-05-16 01:54:19 +0900

    next-auth

---
+++ frontend/auth.ts
@@ -0,0 +1,50 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { verifyPassword } from "./lib/password-utils";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  useSecureCookies: process.env.NODE_ENV === "production",
+  trustHost: true,
+  secret: process.env.AUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "이메일", type: "email" },
+        password: { label: "비밀번호", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials || !credentials.email || !credentials.password) {
+          throw new Error("이메일과 비밀번호를 입력해주세요.");
+        }
+
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+
+        const user = await prisma.user.findUnique({
+          where: { email },
+        });
+
+        if (!user) {
+          throw new Error("존재하지 않는 이메일입니다.");
+        }
+
+        if (!verifyPassword(password, user.hashedPassword as string)) {
+          throw new Error("비밀번호가 일치하지 않습니다.");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {},
+  callbacks: {},
+});
