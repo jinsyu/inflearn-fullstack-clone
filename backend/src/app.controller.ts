2fbe297633761ce8d84b212b5f5a3658f0cb9106
jinsyu <jinsyu.com@gmail.com>
2025-05-16 01:01:15 +0900

    prisma, docker, gitignore

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaClient } from 'generated/prisma';
 
 @Controller()
 export class AppController {
@@ -7,6 +8,8 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    const prisma = new PrismaClient();
+
     return this.appService.getHello();
   }
 }
